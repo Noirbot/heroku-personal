7a0bcce25204bccc5a41d99bf3eacd3eebae8873
Perry Shuman <fortytwofortytwo@gmail.com>
2013-07-04 17:16:39 -0500

    Adding in jade support and pages.

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require("express");
 var app = express();
+var publicFolder = __dirname + '/public'
+
 app.use(express.logger());
+app.use(express.static(publicFolder));
+
+app.set('view engine', 'jade');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('game');
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
